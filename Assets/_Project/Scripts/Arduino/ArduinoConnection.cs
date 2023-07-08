004e4c5fe63fba84bf23b8984c853940dc8765d1
Martin Work <martin.kloeckener@gmail.com>
2023-07-08 17:23:29 +0200

    fixed something that wasn't broken in the first place...

--- Assets/_Project/Scripts/Arduino/ArduinoConnection.cs
+++ Assets/_Project/Scripts/Arduino/ArduinoConnection.cs
@@ -3,20 +3,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
+using Wichtel.Extensions;
 using Wichtel.Utilities;
+
 
 public class ArduinoConnection : MonoBehaviour
 {
     private SerialPort _dataStream;
     private bool _isStreaming = false;
     [SerializeField] private int readTimeout = 50;
-    
+
     public static event Action<string> onNewMessage = delegate(string message) {  };
     public static event Action<string> onNewArduinoError = delegate(string errorMessage) {  };
 
     private void Start()
     {
-        string serialPort = StreamingAssetsUtilities.ReadTextFile("serialPort.txt");
+        string serialPort = string.Empty;
+        
+        //try to automatically get the right COM port
+        bool foundArduinoPort = false;
+        #if UNITY_STANDALONE_WIN
+        try
+        {
+            string[] portInfos = SerialPortUtilities.GetSerialPortsInfos();
+            foreach (string portInfo in portInfos)
+            {
+                if (!portInfo.Contains("Arduino") || !portInfo.Contains("COM")) continue;
+                
+                int comNumber = int.MinValue;
+                string textAfterCom = portInfo.TextAfter("COM");
+                for (int i = textAfterCom.Length - 1; i >= 1; i--)
+                {
+                    string substring = textAfterCom.Substring(0, i);
+                    int.TryParse(substring, out comNumber);
+                }
+                    
+                //if was able to parse com number
+                if (comNumber != int.MinValue)
+                {
+                    serialPort = $"COM{comNumber}";
+                    foundArduinoPort = true;
+                    Debug.Log($"Found Arduino at port {serialPort}");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            // ignored
+        }
+        #endif
+
+        //if it wasn't possible to find the arduino port, use the one from the streaming assets instead
+        if(!foundArduinoPort) serialPort = StreamingAssetsUtilities.ReadTextFile("serialPort.txt");
+        
+        //create data stream and open connection
         _dataStream = new SerialPort(serialPort, 9600);
         OpenConnection();
     }
@@ -45,22 +85,36 @@ public class ArduinoConnection : MonoBehaviour
     
     private void OpenConnection()
     {
-        _isStreaming = true;
-        _dataStream.ReadTimeout = readTimeout;
-        _dataStream.Open();
+        try
+        {
+            _dataStream.ReadTimeout = readTimeout;
+            _dataStream.Open();
+            _isStreaming = true;
+        }
+        catch (Exception exception)
+        {
+            Debug.LogError($"ArduinoError: {exception}");
+            onNewArduinoError?.Invoke(exception.Message);
+        }
     }
 
     private string ReadSerialPort()
     {
-        _dataStream.ReadTimeout = readTimeout;
-
         try
         {
+            _dataStream.ReadTimeout = readTimeout;
             string message = _dataStream.ReadLine();
             return message;
         }
-        catch (TimeoutException exception)
+        catch (TimeoutException timeoutException)
         {
+            //TimeoutException isn't an error in this case because the Arduino doesn't respond as often as every frame
+            return null;
+        }
+        catch (Exception exception)
+        {
+            Debug.LogError($"ArduinoError: {exception}");
+            onNewArduinoError?.Invoke(exception.Message);
             return null;
         }
     }
